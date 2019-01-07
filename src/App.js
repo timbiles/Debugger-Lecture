bbfbeea7d8a31731d369a4dae937206440219a7d
Tim Biles <timbilestimbiles@gmail.com>
2019-01-07 15:07:09 -0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <header>
+        <h1>Styling Debugg</h1>
+
+      </header>
+        <h4>This page has 3 major goals. </h4>
+        <ol>
+          <li>Navbar</li>
+          <li>Position/display boxes</li>
+          <li>Media Queries</li>
+        </ol>
+
+        <div>
+          <nav>Number 1</nav>
+          <p>Using only the Google developers console, make the above navbar stay on the top of the screen. Don't change the styling of the nav itself (the way it looks). </p>
+        </div>
+
+        <div className='num_2'>
+          <nav>Number 2</nav>
+          <p>Look at the following containers. Only using the console, make them look like the following image (Click the button). Make sure and transfer your work over to the code before you refresh the page!</p>
+          <a href="https://i.stack.imgur.com/UDrOe.png" target='blank'>click me</a>
+          <section>
+            <div>A</div>
+            <div>B</div>
+            <div>C</div>
+            <div>D</div>
+            <div>E</div>
+          </section>
+        </div>
+        <div>
+          <nav>Number 3</nav>
+          <p>Assign/fix the media queries to work for 3 different views (phone: 414px width, tablet: 768px, laptop: 1500px)</p>
+        </div>
+     </div>
     );
   }
 }
